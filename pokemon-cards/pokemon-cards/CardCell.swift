f706235e627fd7f7a4a42a0ca3aec3b7df2d90a1
Gökhan Cihan <cihangokhan@gmail.com>
2022-11-11 20:38:59 +0300

    load card image and data

---
+++ pokemon-cards/pokemon-cards/CardCell.swift
@@ -0,0 +1,13 @@
+//
+//  CardCell.swift
+//  pokemon-cards
+//
+//  Created by Gökhan on 10.11.2022.
+//
+
+import UIKit
+
+class CardCell: UICollectionViewCell {
+    @IBOutlet var cardImage: UIImageView!
+    @IBOutlet var pokemonName: UILabel!
+}
--- pokemon-cards/pokemon-cards/ViewController.swift
+++ pokemon-cards/pokemon-cards/ViewController.swift
@@ -7,28 +7,59 @@
 
 import UIKit
 
-class ViewController: UIViewController, URLSessionDelegate {
-
-    var cardList = [PokemonCard.Comparable]()
+class ViewController: UICollectionViewController, URLSessionDelegate {
+    var cardList: [PokemonCard.Comparable]? = nil
+    let session = URLSession(configuration: URLSessionConfiguration.default)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let session = URLSession(configuration: URLSessionConfiguration.default)
-        
         let url = URL(string: "https://api.pokemontcg.io/v1/cards?hp=gte99")!
-        
 
-        getData(with: session, from: url)
-        
+        getData(from: url)
     }
     
-    func getData(with session: URLSession, from url: URL) {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cardList?.count ?? 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as? CardCell else {
+            fatalError("Unable to dequeue CardCell.")
+        }
         
+        if let cardList = cardList {
+            var card = cardList[indexPath.item]
+            
+            let task = session.dataTask(with: URL(string: card.imageUrl)!) {
+                (data, response, error) in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    print("not 200")
+                    return
+                }
+                
+                guard let data = data else{
+                    print(error.debugDescription)
+                    return
+                }
+                
+                DispatchQueue.main.async {
+                    cell.cardImage.image = UIImage(data: data)
+                    cell.pokemonName.text = card.name
+                }
+                
+            }
+            task.resume()
+        }
+        
+        return cell
+    }
+    
+    func getData(from url: URL) {
         let task = session.dataTask(with: url) {
             (data, response, error) in
-            
             guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                print("not 200 get data")
                 return
             }
             
@@ -38,24 +69,22 @@ class ViewController: UIViewController, URLSessionDelegate {
             }
             
             self.parse(data)
-            
         }
-        
         task.resume()
     }
     
     func parse(_ json: Data) {
+        var templist = [PokemonCard.Comparable]()
         let decoder = JSONDecoder()
-        
         if let result = try? decoder.decode(CardsInfo.self, from: json){
             for card in result.cards {
-                cardList.append(card.comparable)
+                    templist.append(card.comparable)
             }
-
+            self.cardList = templist
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
         }
-        
-        print(cardList)
-
     }
     
 }
