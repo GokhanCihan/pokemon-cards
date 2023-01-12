235668870e201618d4c8cd497b0324682e423060
Gökhan Cihan <cihangokhan@gmail.com>
2023-01-13 02:35:15 +0300

    create a manager for handling UserDefaults

---
+++ pokemon-cards/pokemon-cards/Services/DefaultsHandler.swift
@@ -0,0 +1,18 @@
+//
+//  DefaultsHandler.swift
+//  pokemon-cards
+//
+//  Created by Gökhan on 13.01.2023.
+//
+
+import Foundation
+import UIKit
+
+struct DefaultsHandler {
+    static var defaults = UserDefaults.standard
+    
+    func updateDefaults() {
+        <#function body#>
+    }
+    
+}
